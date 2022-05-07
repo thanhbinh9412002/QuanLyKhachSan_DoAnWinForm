ceb412e10b5bd812dcf9c3f30bfc2a570f0acbeb
ThachQuang <20133083@student.hcmute.edu.vn>
2022-05-07 08:48:00 +0700

    Sửa dataprovider

--- QuanLyKhachSan_DoAnWindow/QuanLyKhachSan_DoAnWindow/DataProvider/DataProvider.cs
+++ QuanLyKhachSan_DoAnWindow/QuanLyKhachSan_DoAnWindow/DataProvider/DataProvider.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 
-namespace BatTapDemo.DataProvider
+namespace QuanLyKhachSan_DoAnWindow.DataProvider
 {
         public class DBConnection
         {
